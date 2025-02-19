3aef859ca0b25b836a9e7fecc0a47304abb020af
sambi <samuelisaacospitiaquintero@gmail.com>
2025-02-18 20:53:09 -0500

    Update Comparetoburbuja.java

--- Comparetoburbuja.java
+++ Comparetoburbuja.java
@@ -16,7 +16,7 @@ class Persona implements Comparable<Persona> {
 
     @Override
     public String toString() {
-        return nombre + " (" + edad + ")";
+        return nombre + "," + edad ;
     }
 }
 
